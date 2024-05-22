b2f58851ee933e2b3cece1411471d08429c71035
rT0mmy <46384146+rT0mmy@users.noreply.github.com>
2024-05-22 14:09:54 +0200

    Update Compose.lua

--- Compose.lua
+++ Compose.lua
@@ -3,67 +3,92 @@
 --!optimize 2
 
 --[=[
-	Returns a function that calls the argument functions in left-right order on an input, passing
-	the return of the previous function as argument(s) to the next.
-		
-	@example
-		local function fry(item)
-			return "fried " .. item
-		end
-		
-		local function cheesify(item)
-			return "cheesy " .. item
-		end
+    A simple API for decorator pattern composition
 
-		local prepare = compose(fry, cheesify)
-		prepare("nachos") --> "cheesy fried nachos"
+    @examples - Can be found on the github repo, in examples section
+    @class Composition<T>
 ]=]
 
 export type CompositionFunction<T> = (T)->T
 type CompositionFunctions<T> = {[number]: CompositionFunction<T>}
 
 export type Composition<T> = {
-    Value: T,
-    Modifiers: CompositionFunctions<T>
+	Value: T,
+	Modifiers: CompositionFunctions<T>,
+	
+	SetValue: (self: Composition<T>, value: T)->(),
+	GetValue: (self: Composition<T>)->T,
+	
+	Get: (self: Composition<T>)->T,
+	
+	Modifier: (self: Composition<T>, f:CompositionFunction<T>)->(),
 }
 
 local Compose = {}
 
+--[=[
+    Creates a new composition
+
+    @param value any
+    @return Composition<T>
+]=]
 Compose.new = function<T>(value: T): Composition<T>
-    return {
-        Value = value,
-        Modifiers = {},
+	return {
+		Value = value,
+		Modifiers = {},
 
-        SetValue = Compose.SetValue,
-        GetValue = Compose.GetValue,
+		SetValue = Compose.SetValue,
+		GetValue = Compose.GetValue,
 
-        Get = Compose.Get,
+		Get = Compose.Get,
 
-        Modifier = Compose.Modifier,
-    }
+		Modifier = Compose.Modifier,
+	}
 end
 
+--[=[
+    Sets the base value to @value
+
+    @param value any
+    @return void
+]=]
 function Compose.SetValue<T>(self: Composition<T>, value: T)
 	self.Value = value
 end
 
-function Compose.GetValue<T>(self: Composition<T>)
+--[=[
+    Returns the value of type <T>
+
+    @return T
+]=]
+function Compose.GetValue<T>(self: Composition<T>): T
 	return self.Value
 end
 
---: <T>((...T -> ...T)[]) -> ...T -> T
+--[=[
+    Adds a modifier to the composition modifier array
+
+    @param f (T)->T
+    @return void
+]=]
+--: <T>((...T -> ...T)[]) -> ...T -> T -- ???
 function Compose.Modifier<T>(self: Composition<T>, f: CompositionFunction<T>)
 	table.insert(self.Modifiers, f)
 end
 
-function Compose.Get<T>(self: Composition<T>)
-    local v = self.Value
+--[=[
+    Returns the composed value
 
-    for i, modifier in ipairs(self.Modifiers) do
-        v = modifier(v)
-    end
+    @return T
+]=]
+function Compose.Get<T>(self: Composition<T>): T
+	local v = self.Value
 
-    return v
+	for i, modifier in ipairs(self.Modifiers) do
+		v = modifier(v)
+	end
+
+	return v
 end
 
 return Compose.new
